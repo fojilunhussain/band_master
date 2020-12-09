7247a81a2c006564bd3acfcbe3ebf81bebbdf70a
Fojilun Hussain <fojilun.hussain@gmail.com>
2020-12-09 12:18:24 +0000

    a mock test

---
+++ app/app.test.ts
@@ -0,0 +1,10 @@
+const supertest = require("supertest");
+const app = require("app");
+
+
+describe('Sample test', () => {
+    it('should test that 2 + 2 != 5', () => {
+        expect(2 + 2).not.toBe(5);
+    });
+});
+
