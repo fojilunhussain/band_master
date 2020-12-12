7fe99f33e95f393f360d163163e0973422a45b7e
Fojilun Hussain <fojilun.hussain@gmail.com>
2020-12-12 22:10:03 +0000

    renamed backend app to requests

--- server.ts
+++
@@ -1,9 +0,0 @@
-import { secret } from './pw';
-
-var mysql = require('mysql');
-
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'fhussain',
-    password: secret
-});
