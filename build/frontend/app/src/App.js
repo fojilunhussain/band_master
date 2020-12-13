1f60d104796938a7621c69460634c2ebc52f1a10
Fojilun Hussain <fojilun.hussain@gmail.com>
2020-12-13 00:28:07 +0000

    fix typescript issues

---
+++ build/frontend/app/src/App.js
@@ -0,0 +1,34 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = __importStar(require("React"));
+require("./App.css");
+function App() {
+    return (React.createElement("div", { className: "App" },
+        React.createElement("header", { className: "App-header" },
+            React.createElement("p", null,
+                "Edit ",
+                React.createElement("code", null, "src/App.js"),
+                " and save to reload."),
+            React.createElement("a", { className: "App-link", href: "https://reactjs.org", target: "_blank", rel: "noopener noreferrer" }, "Learn React"))));
+}
+exports.default = App;
+//# sourceMappingURL=App.js.map
--- src/frontend/app/src/App.tsx
+++ src/frontend/app/src/App.tsx
@@ -1,3 +1,4 @@
+import * as React from 'React';
 import './App.css';
 
 function App() {
